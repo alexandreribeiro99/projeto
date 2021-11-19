6e819e0aebf2515a667c5600a71611564ccde6e5
Alexandre Ribeiro <alexandreribeirocontato@gmail.com>
2021-11-18 22:05:47 -0300

    Começando layout

--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -10,11 +10,9 @@ export class ItemsService {
 constructor(private http: HttpClient) { }
 
 estaurl: string = "https://api.github.com/search/repositories?q=language:Java&sort=stars&%20page=1";
-deu: string = "https://api.github.com/users/alexandreribeiro99/repos";
+
 
   listarItems(): Observable<any>{
-    //https://api.github.com/search/repositories?q=language:Java&sort=stars&%20page=1
-    //https://api.github.com/users/alexandreribeiro99/repos
       return this.http.get(this.estaurl);
   }
 
--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,6 +1,7 @@
 import { ItemsService } from './../items.service';
 import { Component, OnInit } from '@angular/core';
 import { Pipe } from '@angular/core';
+import { DoBootstrap } from '@angular/core';
 
 @Component({
   selector: 'app-items',
